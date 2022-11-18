af7fa7c26799a4a111bcbac43f98d9521407467c
nabielmakareem <nabielmakareem10@gmail.com>
2022-11-18 16:52:42 +0700

    Add project files.

---
+++ double_link_list/Program.cs
@@ -0,0 +1,18 @@
+﻿using System
+
+namespace doubble_linked_list
+{
+    class Node
+    {
+        /*node class represent the node of doubly linked list
+         * it consists of the information part and links to
+         * it succesting and precenting 
+         * in tern of next and previous */
+        public int noMhs;
+        public string name;
+        // point to the sucedding node
+        public Node next;
+        public Node prev;
+
+    }
+}
